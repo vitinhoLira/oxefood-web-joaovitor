fe4c1c010bb96c834a99d6f09da2db22ada5c18e
vitinhoLira <contato.joaovitorlira@hotmail.com>
2024-02-29 01:48:47 -0300

    erro resolvido

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
-import Home from './views/home/Home';
+import Home from './views/Home/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      
-      <div style={{ marginTop: '6%' }}>
-        <Segment vertical color='grey' size='tiny' textAlign='center'>
-          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
-        </Segment>
-      </div>
+    return ( <
+        div className = "App" >
+        <
+        Home / >
 
-    </div>
-  );
+        <
+        div style = {
+            { marginTop: '6%' }
+        } >
+        <
+        Segment vertical color = 'grey'
+        size = 'tiny'
+        textAlign = 'center' >
+        &
+        copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes <
+        /Segment> < /
+        div >
+
+        <
+        /div>
+    );
 }
 
 export default App;
